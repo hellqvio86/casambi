4d59211e812356f6be70a12fd3c975e6916ee456
Olof Hellqvist <olof.hellqvist@gmail.com>
2020-05-03 11:44:54 +0200

    Adding readme to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
+
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'casambi',
   packages = find_packages('src'),
   package_dir = {'': 'src'},
-  version = '0.0141',
+  version = '0.0142',
   license='MIT',
   description = 'Library to control Casambi light through cloudapi',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
   author = 'Olof Hellqvist',
   author_email = 'olof.hellqvist@gmail.com',
   url = 'https://github.com/olofhellqvist/casambi',
